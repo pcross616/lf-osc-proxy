687a3a2772cd02e9fae0247368646e8f13fe99e6
Peter Crossley <Peter.Crossley@Webtrends.com>
2014-04-25 00:04:42 -0700

    Initial Commit 1.0 version of LightFactory OSC Proxy

---
+++ src/main/java/com/xley/lfosc/OSCProxyThread.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2014. Peter Crossley
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.xley.lfosc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OSCProxyThread extends Thread {
+    private final static AtomicInteger count = new AtomicInteger();
+    private Socket socket = null;
+
+    public OSCProxyThread(Socket socket) {
+        super("OSCProxyThread - " + count.incrementAndGet());
+        this.socket = socket;
+    }
+
+    public void run() {
+        OSCProxy.logger.info("OSC connection established. Remote Address: " + socket.getInetAddress().getHostAddress());
+        try (
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));
+        ) {
+            String inputLine, outputLine;
+            OSCProxyProtocol opp = new OSCProxyProtocol();
+            while (socket.isConnected() && (inputLine = in.readLine()) != null) {
+                inputLine = inputLine.trim();
+                OSCProxy.logger.trace(">> " + inputLine);
+                outputLine = opp.processInput(inputLine);
+                out.println(outputLine);
+                OSCProxy.logger.trace("<< " + outputLine);
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            OSCProxy.logger.info("OSC connection disconnected. Remote Address: " + socket.getInetAddress().getHostAddress());
+        }
+    }
+}
---
+++ src/test/java/com/xley/lfosc/test/TestOSCListener.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014. Peter Crossley
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.xley.lfosc.test;
+
+import com.illposed.osc.OSCListener;
+import com.illposed.osc.OSCMessage;
+import org.junit.Ignore;
+
+import java.util.Date;
+
+@Ignore
+public class TestOSCListener implements OSCListener {
+
+    private boolean messageReceived = false;
+    private Date receivedTimestamp = null;
+
+    public Date getReceivedTimestamp() {
+        return receivedTimestamp;
+    }
+
+    public boolean isMessageReceived() {
+        return messageReceived;
+    }
+
+    @Override
+    public void acceptMessage(Date time, OSCMessage message) {
+        messageReceived = true;
+        receivedTimestamp = time;
+    }
+}
