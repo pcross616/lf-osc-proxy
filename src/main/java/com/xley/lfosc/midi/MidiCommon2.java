e6472e23564d2dcffa9757caf6c8d0efe81ce992
Peter Crossley <peterc@xley.com>
2015-01-02 00:30:02 -0800

    fixing windows stupid case issue.

---
+++ src/main/java/com/xley/lfosc/midi/MidiCommon2.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2014. Peter Crossley
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.xley.lfosc.midi;
+
+import com.xley.lfosc.util.LogUtil;
+
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+
+public class MidiCommon2 {
+
+    public static MidiDevice.Info[] listDevices(boolean bForInput,
+                                                boolean bForOutput,
+                                                boolean bVerbose) {
+        if (bForInput && !bForOutput) {
+            LogUtil.info(MidiProtocol2.resources.getString("midi.common.available.in.devices"));
+        } else if (!bForInput && bForOutput) {
+            LogUtil.info(MidiProtocol2.resources.getString("midi.common.available.out.devices"));
+        } else {
+            LogUtil.info(MidiProtocol2.resources.getString("midi.common.available.devices"));
+        }
+
+        MidiDevice.Info[] aInfos = MidiSystem.getMidiDeviceInfo();
+        for (int i = 0; i < aInfos.length; i++) {
+            try {
+                MidiDevice device = MidiSystem.getMidiDevice(aInfos[i]);
+                boolean bAllowsInput = (device.getMaxTransmitters() != 0);
+                boolean bAllowsOutput = (device.getMaxReceivers() != 0);
+                if ((bAllowsInput && bForInput) ||
+                        (bAllowsOutput && bForOutput)) {
+                    if (bVerbose) {
+                        LogUtil.trace("" + i + "  "
+                                + (bAllowsInput ? MidiProtocol2.resources.getString("midi.common.in") + " " : "   ")
+                                + (bAllowsOutput ? MidiProtocol2.resources.getString("midi.common.out") + " " : "    ")
+                                + aInfos[i].getName() + ", "
+                                + aInfos[i].getVendor() + ", "
+                                + aInfos[i].getVersion() + ", "
+                                + aInfos[i].getDescription());
+                    } else {
+                        LogUtil.info("" + i + "  " + aInfos[i].getName());
+                    }
+                }
+            } catch (MidiUnavailableException e) {
+                LogUtil.trace(MidiCommon2.class, e);
+            }
+        }
+        if (aInfos.length == 0) {
+            LogUtil.info(MidiProtocol2.resources.getString("midi.common.no.devices"));
+        }
+        return aInfos;
+    }
+
+
+    /**
+     * Retrieve a MidiDevice.Info for a given name.
+     * <p/>
+     * This method tries to return a MidiDevice.Info whose name
+     * matches the passed name. If no matching MidiDevice.Info is
+     * found, null is returned.  If bForOutput is true, then only
+     * output devices are searched, otherwise only input devices.
+     *
+     * @param strDeviceName the name of the device for which an info
+     *                      object should be retrieved.
+     * @param bForOutput    If true, only output devices are
+     *                      considered. If false, only input devices are considered.
+     * @return A MidiDevice.Info object matching the passed device
+     * name or null if none could be found.
+     */
+    public static MidiDevice.Info getMidiDeviceInfo(String strDeviceName, boolean bForOutput) {
+        MidiDevice.Info[] aInfos = MidiSystem.getMidiDeviceInfo();
+        for (MidiDevice.Info aInfo : aInfos) {
+            if (aInfo.getName().equals(strDeviceName)) {
+                try {
+                    MidiDevice device = MidiSystem.getMidiDevice(aInfo);
+                    boolean bAllowsInput = (device.getMaxTransmitters() != 0);
+                    boolean bAllowsOutput = (device.getMaxReceivers() != 0);
+                    if ((bAllowsOutput && bForOutput) || (bAllowsInput && !bForOutput)) {
+                        return aInfo;
+                    }
+                } catch (MidiUnavailableException e) {
+                    LogUtil.trace(MidiCommon2.class, e);
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Retrieve a MidiDevice.Info by index number.
+     * This method returns a MidiDevice.Info whose index
+     * is specified as parameter. This index matches the
+     * number printed in the listDevices method.
+     * If index is too small or too big, null is returned.
+     *
+     * @param index the index of the device to be retrieved
+     * @return A MidiDevice.Info object of the specified index
+     * or null if none could be found.
+     */
+    public static MidiDevice.Info getMidiDeviceInfo(int index) {
+        MidiDevice.Info[] aInfos = MidiSystem.getMidiDeviceInfo();
+        if ((index < 0) || (index >= aInfos.length)) {
+            return null;
+        }
+        return aInfos[index];
+    }
+}
---
+++ src/main/java/com/xley/lfosc/midi/MidiProtocol2.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) 2014. Peter Crossley
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.xley.lfosc.midi;
+
+import com.xley.lfosc.IProtocol;
+import com.xley.lfosc.IProtocolData;
+import com.xley.lfosc.ProtocolException;
+import com.xley.lfosc.impl.BaseProtocol;
+import com.xley.lfosc.impl.SimpleProtocolData;
+import com.xley.lfosc.midi.impl.LearnCommand;
+import com.xley.lfosc.util.LogUtil;
+
+import javax.sound.midi.*;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MidiProtocol2 extends BaseProtocol implements IProtocol {
+
+    /**
+     * The constant resources.
+     */
+    public static final ResourceBundle resources = ResourceBundle.getBundle(MidiProtocol2.class.getSimpleName(),
+            Locale.getDefault());
+
+    /**
+     * The constant midi address pattern.
+     */
+    protected static final Pattern midiAddress = Pattern.compile("^\\/(\\w*)\\/(\\w*)\\/(\\d:\\d:\\d)($|\\?.*$)",
+            Pattern.CASE_INSENSITIVE + Pattern.MULTILINE);
+
+    private static final int MIDI_EVENT_PROTOCOL = 1;
+    private static final int MIDI_EVENT_DEVICE = 2;
+    private static final int MIDI_EVENT_NOTE = 3;
+
+    private static final String DEFAULT_DEVICE = "default";
+
+    @Override
+    public Object process(IProtocolData data) throws ProtocolException {
+        String device = (String) data.getTarget();
+        String[] note = ((String) data.getOperation()).split(":");
+        return _process(Integer.parseInt(note[0]), Integer.parseInt(note[1]), Integer.parseInt(note[2]), device);
+    }
+
+    @Override
+    public IProtocolData createProtocolData(Object value) {
+        if (value instanceof CharSequence) {
+            Matcher matches = midiAddress.matcher((CharSequence) value);
+            //find the address and verify
+            if (matches.find()) {
+                LogUtil.debug(getClass(), resources.getString("midi.event.valid"));
+                String protocol = matches.group(MIDI_EVENT_PROTOCOL);
+                String device = matches.group(MIDI_EVENT_DEVICE);
+                String note = matches.group(MIDI_EVENT_NOTE);
+
+                return new SimpleProtocolData(protocol, device, note, null);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public IProtocolData configureProtocolData(IProtocolData data) {
+        if (data.getOperation() instanceof String) {
+            String operation = (String) data.getOperation();
+            if (operation.startsWith("/")) {
+                ((SimpleProtocolData) data).setOperation(((String) data.getOperation()).substring(1));
+            }
+        }
+        return super.configureProtocolData(data);
+    }
+
+    private Object _process(int nKey, int nVelocity, int nDuration, String deviceName) throws ProtocolException {
+        int nChannel = 0;
+
+        nKey = Math.min(127, Math.max(0, nKey));
+        nVelocity = Math.min(127, Math.max(0, nVelocity));
+        nDuration = Math.max(0, nDuration);
+
+
+        MidiDevice outputDevice = null;
+        Receiver receiver = null;
+        if (deviceName != null && !DEFAULT_DEVICE.equalsIgnoreCase(deviceName)) {
+            MidiDevice.Info info = MidiCommon2.getMidiDeviceInfo(deviceName, true);
+            if (info == null) {
+                throw new ProtocolException(MessageFormat.format(resources.getString("midi.device.not_found"),
+                        deviceName));
+            }
+            try {
+                outputDevice = MidiSystem.getMidiDevice(info);
+                LogUtil.trace(getClass(), MessageFormat.format(resources.getString("midi.device.output_name"),
+                        outputDevice));
+                if (!outputDevice.isOpen()) {
+                    outputDevice.open();
+                }
+            } catch (MidiUnavailableException e) {
+                LogUtil.error(MidiProtocol2.class, e);
+            }
+
+            if (outputDevice == null) {
+                throw new ProtocolException(MessageFormat.format(resources.getString("midi.device.output_unavailable"),
+                        deviceName));
+            }
+            try {
+                receiver = outputDevice.getReceiver();
+            } catch (MidiUnavailableException e) {
+                LogUtil.error(MidiProtocol2.class, e);
+            }
+        } else {
+            try {
+                receiver = MidiSystem.getReceiver();
+            } catch (MidiUnavailableException e) {
+                LogUtil.error(MidiProtocol2.class, e);
+            }
+        }
+        if (receiver == null) {
+            throw new ProtocolException(MessageFormat.format(resources.getString("midi.device.input_unavailable"),
+                    deviceName));
+        }
+
+        LogUtil.debug(getClass(), MessageFormat.format(resources.getString("midi.device.input_name"), deviceName));
+
+		/*	Here, we prepare the MIDI messages to send.
+            Obviously, one is for turning the key on and
+			one for turning it off.
+		*/
+        ShortMessage onMessage = null;
+        ShortMessage offMessage = null;
+        try {
+            onMessage = new ShortMessage();
+            offMessage = new ShortMessage();
+            onMessage.setMessage(ShortMessage.NOTE_ON, nChannel, nKey, nVelocity);
+            offMessage.setMessage(ShortMessage.NOTE_OFF, nChannel, nKey, 0);
+
+        } catch (InvalidMidiDataException e) {
+            LogUtil.error(MidiProtocol2.class, e);
+        }
+
+		/*
+         *	Turn the note on
+		 */
+        LogUtil.trace(getClass(), MessageFormat.format(resources.getString("midi.note.on"),
+                onMessage.getStatus(),
+                onMessage.getData1(),
+                onMessage.getData2()));
+
+        receiver.send(onMessage, -1);
+
+		/*
+         *	Wait for the specified amount of time
+		 *	(the duration of the note).
+		 */
+        try {
+            Thread.sleep(nDuration);
+        } catch (InterruptedException e) {
+            LogUtil.error(MidiProtocol2.class, e);
+        }
+
+		/*
+		 *	Turn the note off.
+		 */
+        LogUtil.trace(getClass(), MessageFormat.format(resources.getString("midi.note.off"),
+                onMessage.getStatus(),
+                onMessage.getData1(),
+                onMessage.getData2()));
+
+        receiver.send(offMessage, -1);
+
+		/*
+		 *	Clean up.
+		 */
+        receiver.close();
+        if (outputDevice != null && outputDevice.getReceivers().size() == 0) {
+            outputDevice.close();
+        }
+        return MessageFormat.format(resources.getString("midi.note.sent"), deviceName, nKey, nVelocity, nDuration);
+    }
+
+    @Override
+    public int command(String commandName, String[] args) throws ProtocolException {
+        switch (commandName) {
+            case "list":
+                MidiCommon2.listDevices(true, true, true);
+                return 0;
+            case "learn":
+                return new LearnCommand().learn(args);
+            default:
+                return super.command(commandName, args);
+        }
+    }
+}
